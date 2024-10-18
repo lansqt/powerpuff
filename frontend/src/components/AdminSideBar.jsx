999de96293d16154996db53b3637bbebbbf5c897
alinaheissel <alnm.hssl@gmail.com>
2024-10-18 11:18:16 +0800

    commit part two

--- frontend/src/components/AdminSideBar.jsx
+++ frontend/src/components/AdminSideBar.jsx
@@ -13,15 +13,25 @@ const AdminSideBar = ({ handleLogout }) => {
     return (
         <div className="sidebar">
             <div className="sidebar-header">
-                <img src="/src/assets/logo.png" alt="Logo" className="logo" />
+                <img src="/src/assets/logo.png" alt="Logo" className="logo-sidebar" />
                 <h2>Book with DDC</h2>
             </div>
             <ul className="nav-list">
-                <li onClick={() => handleNavigation('/admin/dashboard')}>Dashboard</li>
-                <li onClick={() => handleNavigation('/admin/appointments')}>Appointments</li>
-                <li onClick={() => handleNavigation('/admin/dentists')}>Dentists</li>
-                <li onClick={() => handleNavigation('/admin/patients')}>Patients</li>
-                <li onClick={() => handleNavigation('/admin/transactions')}>Transactions</li>
+                <li onClick={() => handleNavigation('/admin')}>
+                    <i className="bi bi-grid"></i> Dashboard
+                </li>
+                <li onClick={() => handleNavigation('/admin/appointments')}>
+                    <i className="bi bi-calendar-check"></i> Appointments
+                </li>
+                <li onClick={() => handleNavigation('/admin/dentists')}>
+                    <i className="bi bi-person-badge"></i> Dentists
+                </li>
+                <li onClick={() => handleNavigation('/admin/patients')}>
+                    <i className="bi bi-people"></i> Patients
+                </li>
+                <li onClick={() => handleNavigation('/admin/transactions')}>
+                    <i className="bi bi-receipt"></i> Transactions
+                </li>
             </ul>
             <div className="logout">
                 <li onClick={handleLogout}>
